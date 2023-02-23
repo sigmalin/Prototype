1fe7531ebb9213711d9fae94c8130057c746604c
sigma-lin-7senses <101098088+sigma-7senses@users.noreply.github.com>
2023-02-23 17:02:03 +0800

    Update backend
    更新後端程式碼

---
+++ backend/sql/mainDB.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS `main_db`
+USE `main_db`;
+
+
+CREATE TABLE IF NOT EXISTS `Users` (
+  `UserID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL DEFAULT '0',
+  `Mail` varchar(50) NOT NULL DEFAULT '0',
+  `Password` varchar(50) NOT NULL,
+  `CreateTime` int(11) NOT NULL COMMENT '帳號建立時間',
+  `UpdateTime` int(11) NOT NULL COMMENT '最後登入時間',
+  UNIQUE KEY `UserID` (`UserID`),
+  UNIQUE KEY `Mail` (`Mail`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='玩家帳號資料';
