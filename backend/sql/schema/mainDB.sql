e7ec55215ac1396480b762f61035022e532ada52
FINGERGAME\FG000343 <sigma.fg343@gmail.com>
2023-07-19 15:17:35 +0800

    update backend

--- backend/sql/schema/mainDB.sql
+++ backend/sql/schema/mainDB.sql
@@ -1,5 +1,15 @@
-CREATE DATABASE IF NOT EXISTS `main_db`;
+CREATE DATABASE IF NOT EXISTS `main_db`
 USE `main_db`;
+
+
+CREATE TABLE IF NOT EXISTS `Bank` (
+  `UserID` int(10) unsigned NOT NULL,
+  `Coin` bigint(20) unsigned zerofill NOT NULL DEFAULT 00000000000000000000 COMMENT '遊戲貨幣 (物品買賣 & 任務達成獲得)',
+  `Faith` bigint(20) unsigned zerofill NOT NULL DEFAULT 00000000000000000000 COMMENT '遊戲貨幣 (成就 & 任務達成獲得)',
+  `Gems` bigint(20) unsigned zerofill NOT NULL DEFAULT 00000000000000000000 COMMENT '商城貨幣',
+  `Treasure` bigint(20) unsigned zerofill NOT NULL DEFAULT 00000000000000000000 COMMENT '商城貨幣 (課金)',
+  UNIQUE KEY `UserID` (`UserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='玩家貨幣資料';
 
 
 CREATE TABLE IF NOT EXISTS `Users` (
@@ -12,3 +22,4 @@ CREATE TABLE IF NOT EXISTS `Users` (
   UNIQUE KEY `UserID` (`UserID`),
   UNIQUE KEY `Token` (`Token`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='玩家帳號資料';
+
