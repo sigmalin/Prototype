df4ab1a264fa9390fcfedb3e0b396313fedffec6
FINGERGAME\FG000343 <sigma.fg343@gmail.com>
2023-07-28 15:26:29 +0800

    update frontend : add example for stream

--- frontend/Assets/gRPC/Scripts/gRpcMain.cs
+++ frontend/Assets/gRPC/Scripts/gRpcMain.cs
@@ -1,6 +1,7 @@
 using System;
 using Grpc.Core;
 using Protobufs.helloWorld;
+using Protobufs.chat;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,13 @@ public class gRpcMain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        helloworld();
+
+        chat();
+    }
+
+    void helloworld()
+    {
         Channel channel = new Channel("127.0.0.1:5033", ChannelCredentials.Insecure);
 
         var client = new HelloWorldService.HelloWorldServiceClient(channel);
@@ -18,5 +26,21 @@ public class gRpcMain : MonoBehaviour
         Debug.Log($"reply = {reply.Msg}");
 
         channel.ShutdownAsync().Wait();
+    }
+
+    async void chat()
+    {
+        Channel channel = new Channel("127.0.0.1:5033", ChannelCredentials.Insecure);
+
+        var client = new ChatService.ChatServiceClient(channel);
+
+        using(var listener = client.Listen(new ListenRequest() { Name = "sigma" }))
+        {
+            while (await listener.ResponseStream.MoveNext())
+            {
+                var result = listener.ResponseStream.Current;
+                Debug.Log($"{result.Name} : {result.Msg}");
+            }
+        }
     }
 }
